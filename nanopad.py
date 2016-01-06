818039dc91451e434a95430f94af4098da85368c
Jacobe2169 <jacques.fize@gmail.com>
2016-01-06 16:31:29 +0100

    First Commit

---
+++ nanopad.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+
+import rtmidi
+import time
+from threading import Thread
+
+class Pad(Thread):
+    """docstring for Pad"""
+
+    def __init__(self):
+        Thread.__init__(self)
+        self.source = rtmidi.MidiIn()
+        self.score = 0.0
+
+    def run(self):
+        if self.source.get_port_count() > 0:
+            self.loop = True
+            self.source.open_port(0)
+            compteur = 0.0
+            acc = 0.0
+            score = 0
+            while(self.loop):
+                time.sleep(0.01)
+                data = self.source.get_message()
+                compteur += 0.01
+                if data:
+                    acc += 1
+                if compteur > 0.4:
+                    self.score = self.score + acc - self.score
+                    # Transformation en Watt
+                    self.score = self.score * 7
+                    acc = 0.0
+                    compteur = 0.0
+                    # print score
+
+        else:
+            return
+
+    def stop(self):
+        self.loop = False
